615fb0386375bd079144f8f01533e479dd2c703c
João Vitor G A Mintz <joaomintzz@gmail.com>
2025-02-20 12:46:23 +0000

    Arquivos java da aula de lab

---
+++ Lab01/Main.java
@@ -0,0 +1,27 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    Cachorro meuPet = new Cachorro("Totó", 2025, "Labrador", "chocolate", true, 15, 0.5, 5);
+		Cachorro meuPet2 = new Cachorro();
+	    Cachorro meuPet3 = new Cachorro("Lulu", 2024, "Pit", "branca", true, 20, 0.8, 5);
+	    
+		System.out.println(meuPet);
+		System.out.println("------------------");
+		/*
+		System.out.println("Dados do meuPet:\n");
+		System.out.println("Nome = " + meuPet.nome);
+		System.out.println("Raça = " + meuPet.raca);
+		System.out.println("Nascimento = " + meuPet.nascimento);
+		System.out.println("Energia = " + meuPet.energia);
+		System.out.println("Acordado = " + meuPet.acordado); 
+		*/
+		meuPet.setEnergia(-10);
+		
+		System.out.println("Energia = " + meuPet.getEnergia());
+		
+		
+		
+		
+	}
+}
