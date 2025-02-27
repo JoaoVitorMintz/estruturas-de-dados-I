69c0aab5401a4ef86ff6573bc8603e071ea71cc6
João Vitor G A Mintz <joaomintzz@gmail.com>
2025-02-27 08:07:34 -0300

    atualização do prof JAVA

--- Lab01/Cachorro.java
+++ Lab01/Cachorro.java
@@ -1,80 +1,17 @@
-public class Cachorro extends Animal {
-    
+public class Cachorro  extends Animal {
+   
     //construtor
-    public Animal(String nome, int nasc, String raca, String cor, boolean acordado, int tamanho, double peso, int energia) {
-
+    public Cachorro(String nome, int nasc, String raca, String cor, 
+                   boolean acordado, int tamanho, double peso, int energia) {
         super(nome, nasc, raca, cor, acordado, tamanho, peso, energia);
-
     }
-
+    
     public Cachorro() {
-
+        super();
+        
     }
     
-    //métodos
-    
-    
-    public String toString() {
-        String s;
-        s = "Nome = " + this.nome 
-        + "\nRaça = " + this.raca
-		+ "\nNascimento = " + this.nascimento
-		+ "\nEnergia = " + this.energia
-		+ "\nAcordado = " + this.acordado;
-		
-		return s;
-    }
-    
-    public void setEnergia(int valor) {
-        if(valor >= 0)
-           this.energia = valor;
-    }
-    
-    public int getEnergia() {
-        return this.energia;
-    }
-
-    public int getNascimento() {
-        return this.nascimento;
-    }
-
-    public String getRaca() {
-        return this.raca;
-    }
-
-    public String getNome() {
-        return this.nome;
-    }
-
-    public boolean getAcordado() {
-        return this.acordado;
-    }
-    
-    public int getTamanho() {
-        return this.tamanho;
-    }
-
-    public double getPeso() {
-        return this.peso;
-    }
-
-    public String getCor() {
-        return this.cor;
-    }
-
-    public void acordar() {
-        acordado = true;
-    }
-    public void dormir() {
-        acordado = false;
-    }
-    public void comer(int quantidade) {
-        energia += quantidade;
-    }
-    public void andar() {
-        energia -= 1;
-    }
-    public void latir() {
+    public void som() {
         System.out.println("Au Au!!");
     }
 }
--- Lab01/Gato.java
+++ Lab01/Gato.java
@@ -1,93 +1,19 @@
-public class Gato extends Animal {
+public class Gato  extends Animal {
 
+    
     //construtor
     public Gato(String nome, int nasc, String raca, String cor, 
                    boolean acordado, int tamanho, double peso, int energia) {
-        
-        this.nome = nome;
-        this.nascimento = nasc;
-        this.raca = raca;
-        this.cor = cor;
-        this.acordado = acordado;
-        this.tamanho = tamanho;
-        this.peso = peso;
-        this.energia = energia;
+        super(nome, nasc, raca, cor, acordado, tamanho, peso, energia);
     }
     
     public Gato() {
-        this.nome = "sem nome";
-        this.raca = "não definida";
+        super();
     }
     
     //métodos
-    
-    
-    public String toString() {
-        String s;
-        s = "Nome = " + this.nome 
-        + "\nRaça = " + this.raca
-		+ "\nNascimento = " + this.nascimento
-		+ "\nEnergia = " + this.energia
-		+ "\nAcordado = " + this.acordado
-        + "\nAssustado = " + this.assustado;
-		
-		return s;
-    }
-    
-    public void setEnergia(int valor) {
-        if(valor >= 0)
-           this.energia = valor;
-    }
-    
-    public int getEnergia() {
-        return this.energia;
-    }
 
-    public int getNascimento() {
-        return this.nascimento;
-    }
-
-    public String getRaca() {
-        return this.raca;
-    }
-
-    public String getNome() {
-        return this.nome;
-    }
-    
-    public String getCor() {
-        return this.cor;
-    }
-
-    public boolean getAcordado() {
-        return this.acordado;
-    }
-
-    public int getTamanho() {
-        return this.tamanho;
-    }
-    
-    public double getPeso() {
-        return this.peso;
-    }
-
-    public boolean getAssustado() {
-        return this.assustado;
-    }
-    
-    public void acordar() {
-        acordado = true;
-    }
-    public void dormir() {
-        acordado = false;
-    }
-    public void comer(int quantidade) {
-        energia += quantidade;
-    }
-    public void andar() {
-        energia -= 1;
-    }
-    public void miar() {
-        System.out.println("Miau");
+    public void som() {
+        System.out.println("Miauuuuuuuu!!");
     }
 }
