b4c8cde9ad80cd71eae1d5241e9e2a9ee42735c8
João Vitor G A Mintz <joaomintzz@gmail.com>
2025-04-22 10:49:48 -0300

    Criando Node com auxílio do professor

---
+++ linkedList/Node.java
@@ -0,0 +1,37 @@
+public class Node<T> {
+	
+	private T data;
+	private Node<T> next;
+	
+	// Costrutores:
+	public Node(T data) {
+		this.data = data;
+		this.next = null;
+	}
+	
+	public Node() {
+		this(null);
+	}
+	
+	// Métodos:
+	public T getData() {
+		return data;
+	}
+	
+	public void setData(T data) {
+		this.data = data;
+	}
+	
+	public Node<T> getNext() {
+		return next;
+	}
+	
+	public void setNext(Node<T> next) {
+		this.next = next;
+	}
+	
+	@Override
+	public String toString() {
+		return "data: " + data + "next: " + (next!=null?next.getData():"null"); 
+	}
+}
